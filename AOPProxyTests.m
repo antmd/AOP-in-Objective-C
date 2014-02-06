f0b73cb9890222a5f3fe6ba492dda05c0562a4f2
Alex Gray <alex@mrgray.com>
2014-02-05 23:14:35 -0500

    setup travos + coveralls.. let's try it

---
+++ AOPProxyTests.m
@@ -0,0 +1,34 @@
+//
+//  AOPProxyTests.m
+//  AOPProxyTests
+//
+//  Created by Alex Gray on 2/5/14.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface AOPProxyTests : SenTestCase
+
+@end
+
+@implementation AOPProxyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
