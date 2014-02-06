dfbd465fa5496481ebda8d4f6c8095b8b3a5855f
Alex Gray <alex@mrgray.com>
2014-02-06 03:18:39 -0500

    futzing with gcov

--- .coveralls.sh
+++ .coveralls.sh
@@ -1,13 +1,26 @@
-#!/bin/bash
+#!/bin/zsh
 
-XCENV=$(xcodebuild -project AOPProxy.xcodeproj -target "AOPProxy (OSX)" -showBuildSettings)
-BUILT_PRODUCTS_DIR="/$(echo $XCENV | grep -i "^\s*BUILT_PRODUCTS_DIR\s=\s" | cut -d/ -f2-)"
-CURRENT_ARCH=x86_64
-OBJECT_FILE_DIR_normal="/$(echo $XCENV | grep -i "^\s*OBJECT_FILE_DIR_normal\s=\s" | cut -d/ -f2-)"
-SRCROOT="/$(echo $XCENV | grep -i "^\s*SRCROOT\s=\s" | cut -d/ -f2-)"
-OBJROOT="/$(echo $XCENV | grep -i "^\s*OBJROOT\s=\s" | cut -d/ -f2-)"
+cd $(dirname $0)
+ZZCONFIGURATION="Debug"
+ZZXCENV=`xcodebuild -project AOPProxy.xcodeproj -target "AOPProxy (OSX)" -showBuildSettings`
+ZZBUILT_PRODUCTS_DIR="/`echo ${ZZXCENV} | grep -i '^\s*BUILT_PRODUCTS_DIR\s=\s' | cut -d/ -f2-`"
+ZZCURRENT_ARCH='x86_64'
+ZZPROJECT_TEMP_DIR="/`echo ${ZZXCENV} | grep -i '^\s*PROJECT_TEMP_DIR\s=\s' | cut -d/ -f2-`"
+# ZZSRCROOT="/$(echo $XCENV | grep -i "^\s*SRCROOT\s=\s" | cut -d/ -f2-)"
+# ZZOBJROOT="/$(echo $XCENV | grep -i "^\s*OBJROOT\s=\s" | cut -d/ -f2-)"
 
-declare -r gcov_dir="${OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
+echo -e "BUILT_PRODUCTS_DIR is $ZZBUILT_PRODUCTS_DIR"
+echo -e "CURRENT_ARCH is $ZZCURRENT_ARCH"
+# echo -e "OBJECT_FILE_DIR_normal is $OBJECT_FILE_DIR_normal"
+# echo -e "SRCROOT is $SRCROOT"
+# echo -e "OBJROOT is $OBJROOT"
+
+
+# declare -r
+gcov_dir="${ZZPROJECT_TEMP_DIR}/${ZZCONFIGURATION}/AOPProxy (OSX).build/Objects-normal/${ZZCURRENT_ARCH}"
+echo -e "gcov_dir is $gcov_dir"
+
+# {OBJECT_FILE_DIR_normal}/${CURRENT_ARCH}/"
 
 ## ======
 
