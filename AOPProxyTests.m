0be9a060a6d6673abe55b2e4e5ffbe6f7bb3e62e
Alex Gray <alex@mrgray.com>
2014-02-05 23:49:23 -0500

    added some tests (that pass)

--- AOPProxyTests.m
+++ AOPProxyTests.m
@@ -1,15 +1,10 @@
-//
-//  AOPProxyTests.m
-//  AOPProxyTests
-//
-//  Created by Alex Gray on 2/5/14.
-//
-//
 
 #import <SenTestingKit/SenTestingKit.h>
+#import "AOPProxy.h"
 
 @interface AOPProxyTests : SenTestCase
-
+@property AOPProxy * proxy;
+@property NSMutableString *testString;
 @end
 
 @implementation AOPProxyTests
@@ -17,18 +12,36 @@
 - (void)setUp
 {
     [super setUp];
+    _testString = @"apple".mutableCopy;
+    _proxy = [AOPProxy proxyWithObject:_testString];
     // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
-- (void)tearDown
-{
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-}
+- (void)tearDown { [super tearDown]; }
 
-- (void)testExample
-{
-    STFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+- (void)testInvocationBlock {
+
+  STAssertEquals  ([_proxy proxiedObject], _testString, @"proxied object should equal our s†ring");
+  STAssertTrue    (_testString.length == 5,             @"test string should start out with length of 5. it is %lu", _testString.length);
+  [_proxy interceptMethodForSelector:@selector(length)
+                    interceptorPoint:InterceptPointEnd
+                               block:^(NSInvocation * i, InterceptionPoint p){
+
+
+    STAssertNotNil(i,                                                         @"Invocation Should exist");
+
+
+
+    STAssertTrue  (p == InterceptPointEnd,                                    @"insertion poin† should be at end!");
+    STAssertTrue  ([i.target isEqual:_testString],                            @"invocation target should be our string!");
+    NSString *selStr = NSStringFromSelector(i.selector);
+    STAssertTrue  ([selStr isEqualToString:@"length"],                        @"selector should be 'length'");
+    STAssertTrue  ([[i.target valueForKey:selStr]unsignedIntegerValue] == 10, @"length should be 10, now.");
+  }];
+
+  [(id)_proxy appendString:@"farts"];
+  NSUInteger length = [(id)_proxy length];
+  STAssertTrue(length == 10, @"length should be 10, now.");
 }
 
 @end
